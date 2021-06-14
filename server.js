e28573a81d18fe87d65ce41cb30bcd915470cb24
divyasingh1 <er.divyasingh1@gmail.com>
2021-06-14 23:12:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('./app');
-var port = 4000;
+var port = process.env.port || 4000;
 var server = app.listen(port,function(){
     console.log("express app listening on port "+ port);
 });
